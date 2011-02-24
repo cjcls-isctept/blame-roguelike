3a9f448228f182c8fb483724aa21c1843d3ced05
dunnolol.b <dunnolol.b@9d02ef5a-ef0a-11de-b92f-5d3588262692>
2011-02-24 16:46:58 +0000

    relocating Blamer project, phase 2

---
+++ BlamerMaven/src/main/scala/forlulz/StateTest.scala
@@ -0,0 +1,24 @@
+package forlulz
+
+import su.msk.dunno.scage.screens.support.tracer.{State}
+
+object StateTest extends Application {
+  /*val state = new State("data1", "hello").put("data2", "world").put("hello", "peace!")
+  new state.check {
+    key("data1") {
+      key("data2") {
+        key(string("data1")) {
+          println(string(string("data1"))+" "+string("data1")+" "+string("data2"))
+        }
+      }
+    }
+    key("data2") {
+      println(3+string)
+    }
+  }
+
+  val new_state = state.filter(str => {
+    str == "data1" || str == "data2" || str== state.getString("data1")
+  })
+  println(new_state)*/
+}
